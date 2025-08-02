8c4f9ecad49c1d97f9d5499301bb1810812e1f99
ChiKhoiCoder <nguyenchikhoi2962005dhpt@gmail.com>
2025-08-02 13:27:09 +0700

    kfj

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+using ZstdSharp.Unsafe;
+
+
+namespace programing
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Nhap n = ");
+            int n = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split();
+            int[] arr = new int[n];
+            for(int i =  0; i < n; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+           
+            var number = from i in arr
+                         where i % 2 == 0
+                         select i;
+          foreach(int i in number)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+   
+}
+
